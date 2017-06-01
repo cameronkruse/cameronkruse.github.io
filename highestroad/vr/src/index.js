508ed8b9028aadf2a5ac622d3cb7542588d63670
cameronkruse <dirtonbaseball@gmail.com>
2017-06-01 11:03:44 -0700

    Revert "one last try"
    This reverts commit 44d7ff56046f07f4d8234d3ad5b0c9571eabdb43.

--- highestroad/vr/src/index.js
+++ highestroad/vr/src/index.js
@@ -3,7 +3,7 @@ import CanvasMap from './canvas-map'
 
 let map=CanvasMap({
   textContainer:document.querySelector('.text'),
-  mapSrc:'../img/map.svg',
+  mapSrc:'./map.svg',
   trailVisitedColor:'#47DBB4',
   fontPresentColor:'#5D5C56',
 }).appendTo('.container')
