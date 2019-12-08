a02e06ff62ba6036926036858fb9a865b5821c72
cameronkruse <cameronwkrus@gmail.com>
2019-12-07 17:38:05 -0800

    photo push 4

--- home/projects/nepal/js/config.js
+++ home/projects/nepal/js/config.js
@@ -12,7 +12,7 @@ var config = {
         {
             id: 'chapter1',
             title: 'Mustang',
-            image: './path/to/image/source.png',
+            image: './images/ckrusenepal-1.jpg',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             location: {
                 center: [83.99787, 29.06679],
