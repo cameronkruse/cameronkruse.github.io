684a842c385102f54bf81805cd627c176a9d0559
cameronkruse <cameronwkrus@gmail.com>
2019-12-11 00:17:23 -0800

    sticky block added
    using scrollama videoMap is the style I'm using right now.

--- home/projects/nepal/js/config.js
+++ home/projects/nepal/js/config.js
@@ -8,7 +8,7 @@ var config = {
     subtitle: 'Climate refugees high in the Himalayas negotiate with our changing planet',
     byline: 'By Cameron Kruse, Tashi Bista, and Corey Robinson',
     footer: 'Source: A National Geographic Project',
-    videoMap: '../videos/chtrimmedsmall.mov',
+    videoMap: 'this is a videoMap text',
     chapters: [
         {
             id: 'chapter1',
@@ -137,6 +137,57 @@ var config = {
                 zoom: 13.03,
                 pitch: 112.8,
                 bearing: 60
+            },
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        }
+    ],
+
+    stickyChapters: [
+        {
+            id: 'chapter6',
+            title: 'Mustang',
+            image: './images/ckrusenepal-109.jpg',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            location: {
+                center: [83.99787, 29.06679],
+                zoom: 12.57,
+                pitch: 98.20,
+                bearing: 60
+            },
+            onChapterEnter: [
+                {
+                    layer: 'tracks1',
+                    opacity: 0
+                }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+        {
+            id: 'chapter7',
+            title: 'Dhey',
+            image: './images/ckrusenepal-95.jpg',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            location: {
+                center: [84.014188, 29.041815],
+                zoom: 17.6,
+                pitch: 0,
+                bearing: 0
             },
             onChapterEnter: [
                 // {
