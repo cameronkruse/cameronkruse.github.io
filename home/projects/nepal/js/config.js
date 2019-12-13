43dcc73c0013a98c0af04ea2aacdb07ff508288c
cameronkruse <cameronwkrus@gmail.com>
2019-12-13 00:14:09 -0800

    got sticky block to appear
    there is a listener that checks for sticky block to be visible or hidden in config file

--- home/projects/nepal/js/config.js
+++ home/projects/nepal/js/config.js
@@ -1,7 +1,7 @@
 var config = {
     style: 'mapbox://styles/highestroad/ck3ozjhyd0myt1cqz0imxcppq',
     accessToken: 'pk.eyJ1IjoiaGlnaGVzdHJvYWQiLCJhIjoiY2lzNjlpa3c3MGQ3cDJ6cDFzMXZpZTNmMCJ9.M1X4AOcuj4n3VT01ze0x5Q',
-    showMarkers: false,
+    showMarkers: true,
     theme: 'dark',
     alignment: 'left',
     title: 'Where To Run When The Glaciers Run Dry',
@@ -9,6 +9,7 @@ var config = {
     byline: 'By Cameron Kruse, Tashi Bista, and Corey Robinson',
     footer: 'Source: A National Geographic Project',
     videoMap: 'this is a videoMap text',
+    something: 'the words here',
     chapters: [
         {
             id: 'chapter1',
@@ -23,15 +24,15 @@ var config = {
             },
             onChapterEnter: [
                 {
-                    layer: 'tracks1',
-                    opacity: 0
+                    // layer: 'tracks1',
+                    // opacity: 0
                 }
             ],
             onChapterExit: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 0
-                // }
+                {
+                    layer: 'tracks1',
+                    opacity: 0
+                }
             ]
         },
         {
@@ -46,14 +47,39 @@ var config = {
                 bearing: 0
             },
             onChapterEnter: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 1
-                // }
+                {
+                    // layer: 'tracks1',
+                    // opacity: 1
+                }
             ],
             onChapterExit: [
                 // {
-                //     layer: 'layer-name',
+                //     layer: 'tracks1',
+                //     opacity: 0
+                // }
+            ]
+        },
+        {
+            id: 'chapter25',
+            title: 'Line should appear beneath showsticky true',
+            image: './images/ckrusenepal-95.jpg',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            showSticky: true,
+            location: {
+                center: [84.014188, 29.041815],
+                zoom: 17.6,
+                pitch: 0,
+                bearing: 0
+            },
+            onChapterEnter: [
+                {
+                    layer: 'tracks1',
+                    opacity: 1
+                }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'tracks1',
                 //     opacity: 0
                 // }
             ]
@@ -69,7 +95,12 @@ var config = {
                 pitch: 0,
                 bearing: 0
             },
-            onChapterEnter: [],
+            onChapterEnter: [
+              {
+                  layer: 'tracks1',
+                  opacity: 1
+              }
+            ],
             onChapterExit: []
         },
         {
@@ -100,7 +131,6 @@ var config = {
             id: 'sticky1',
             title: 'Water fed Mill',
             image: './images/ckrusenepal-62.jpg',
-            stickyimage: './images/ckrusenepal-31.jpg',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             location: {
                 center: [83.99449, 29.04885],
@@ -108,12 +138,6 @@ var config = {
                 pitch: -162,
                 bearing: 9
             },
-            sticky: [
-                {
-                    layer: 'tracks1',
-                    opacity: 1
-                }
-            ],
             onChapterEnter: [
                 {
                     layer: 'tracks1',
@@ -132,62 +156,12 @@ var config = {
             title: 'Glaciers',
             image: './images/ckrusenepal-105.jpg',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            hideSticky: true,
             location: {
                 center: [84.09096, 28.91339],
                 zoom: 13.03,
                 pitch: 112.8,
                 bearing: 60
-            },
-            onChapterEnter: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 1
-                // }
-            ],
-            onChapterExit: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 0
-                // }
-            ]
-        }
-    ],
-
-    stickyChapters: [
-        {
-            id: 'chapter6',
-            title: 'Mustang',
-            image: './images/ckrusenepal-109.jpg',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            location: {
-                center: [83.99787, 29.06679],
-                zoom: 12.57,
-                pitch: 98.20,
-                bearing: 60
-            },
-            onChapterEnter: [
-                {
-                    layer: 'tracks1',
-                    opacity: 0
-                }
-            ],
-            onChapterExit: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 0
-                // }
-            ]
-        },
-        {
-            id: 'chapter7',
-            title: 'Dhey',
-            image: './images/ckrusenepal-95.jpg',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            location: {
-                center: [84.014188, 29.041815],
-                zoom: 17.6,
-                pitch: 0,
-                bearing: 0
             },
             onChapterEnter: [
                 // {
