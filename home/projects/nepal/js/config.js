d4ad876b3086384f73939b372d84c8ff20da3526
Cameron Kruse <dirtonbaseball@gmail.com>
2020-03-06 01:43:43 -0800

    modifying to make scrollydrive work try 3

--- home/projects/nepal/js/config.js
+++ home/projects/nepal/js/config.js
@@ -1,3 +1,24 @@
+// Number of slides that will drive (more = smoother)
+// If this doesn't match the number of slides named 'drive-slide' in config below you will not complete the full journey
+var driveSlides = 1;
+
+// Number of points on drive route (more = higher quality, but slower to process)
+var driveSmoothness = 200;
+
+// Value used to drive
+var driveTime = driveSlides*driveSmoothness;
+
+// Do you want to follow the point? True = follow
+var followPoint = true;
+
+// ...If so, what zoom, pitch, and bearing should be used to follow?
+var followZoomLevel = 14;
+var followBearing = 29;
+var followPitch = 0;
+
+// to add 'driving' slides just make sure to add 'drive to beginning of slide id'
+// you also need to add a running total to the end of each 'drive-slide', (ex. drive-slide-0, drive-slide-1, drive-slide-2, etc.)
+
 var config = {
     style: 'mapbox://styles/highestroad/ck7cqndn70h3m1ikx53ikytqs',
     accessToken: 'pk.eyJ1IjoiaGlnaGVzdHJvYWQiLCJhIjoiY2lzNjlpa3c3MGQ3cDJ6cDFzMXZpZTNmMCJ9.M1X4AOcuj4n3VT01ze0x5Q',
@@ -35,13 +56,13 @@ var config = {
             ],
             onChapterExit: [
                 {
-                    layer: 'tracks1',
+                    layer: 'chambale-dhey-road2',
                     opacity: 0
                 }
             ]
         },
         {
-            id: 'chapter2',
+            id: 'drive-chapter2',
             title: 'Dhey',
             image: './images/ckrusenepal-95.jpg',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
@@ -71,7 +92,7 @@ var config = {
             ]
         },
         {
-            id: 'chapter25',
+            id: 'drive-chapter25',
             title: 'Line should appear beneath showsticky is on',
             image: './images/ckrusenepal-95.jpg',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
@@ -89,8 +110,8 @@ var config = {
             },
             onChapterEnter: [
                 {
-                    layer: 'tracks1',
-                    opacity: 1
+                    // layer: 'tracks1',
+                    // opacity: 1
                 }
             ],
             onChapterExit: [
@@ -103,7 +124,7 @@ var config = {
         {
             id: 'chapter3',
             title: 'Chambaleh',
-            image: './images/ckrusenepal-1.jpg',
+            image: './images/ckrusenepal-37.jpg',
             description: 'Copy these sections to add to your story.',
             photoBlock: true,
             videoBlock: false,
@@ -119,9 +140,9 @@ var config = {
             },
             onChapterEnter: [
               {
-                  layer: 'tracks1',
-                  opacity: 1
-              }
+              //     layer: 'chambale-dhey-road2',
+              //     opacity: 1
+              // }
             ],
             onChapterExit: []
         },
@@ -144,8 +165,8 @@ var config = {
             },
             onChapterEnter: [
                 {
-                    layer: 'tracks1',
-                    opacity: 1
+                    // layer: 'tracks1',
+                    // opacity: 1
                 }
             ],
             onChapterExit: [
@@ -174,14 +195,14 @@ var config = {
             },
             onChapterEnter: [
                 {
-                    layer: 'tracks1',
-                    opacity: 1
+                    // layer: 'tracks1',
+                    // opacity: 1
                 }
             ],
             onChapterExit: [
                 {
-                    layer: 'tracks1',
-                    opacity: 0
+                    // layer: 'tracks1',
+                    // opacity: 0
                 }
             ]
         },
